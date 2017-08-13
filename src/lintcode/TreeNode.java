2d5afb6faa6aaf60b6d2415ed9e1cdb28f848d82
CoderKiSoo <coderkisoo@gmail.com>
2017-08-13 13:38:19 +0800

    commit： 二叉树的层序遍历： TreeNode struct

---
+++ src/lintcode/TreeNode.java
@@ -0,0 +1,16 @@
+package lintcode;
+
+/**
+ * Created by kangqizhou on 2017/8/13.
+ */
+public class TreeNode {
+    public int value;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode parent;
+
+    public TreeNode(int value){
+        this.value = value;
+    }
+
+}
