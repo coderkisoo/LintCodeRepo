d21ee5b86389b40fbec06bbddfccf6c860cdc2e4
coderkisoo <coderkisoo@gmail.com>
2017-09-01 18:12:45 +0800

    Commit: Builder模式

---
+++ src/DesignPattern/BuilderClient.java
@@ -0,0 +1,34 @@
+package DesignPattern;
+
+/**
+ * Created by shs1329 on 2017/9/1.
+ */
+public class BuilderClient {
+    int value;
+    int temp;
+
+    public BuilderClient(Builder builder) {
+        this.value = builder.value;
+        this.temp = builder.temp;
+    }
+
+    public static class Builder{
+        int value;
+        int temp;
+
+        public Builder value(int value){
+            this.value = value;
+            return this;
+        }
+
+
+        public Builder temp(int temp){
+            this.temp = temp;
+            return this;
+        }
+
+        public BuilderClient build(){
+            return new BuilderClient(this);
+        }
+    }
+}
