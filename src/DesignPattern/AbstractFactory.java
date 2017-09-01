2b7ec937ec0ed9b4bcc0c4f8913ea8ddd10c7445
coderkisoo <coderkisoo@gmail.com>
2017-09-01 17:59:04 +0800

    Commit: 抽象工厂。 建立抽象工厂，继承抽象工厂

--- src/DesignPattern/AbstractFactory.java
+++ src/DesignPattern/AbstractFactory.java
@@ -3,6 +3,38 @@ package DesignPattern;
 /**
  * Created by shs1329 on 2017/9/1.
  */
+
+/***
+ * 抽象工厂模式
+ */
 public abstract class AbstractFactory {
 
+    public interface Product{}
+
+    abstract Product produce();
+
+}
+
+class FactoryA extends AbstractFactory{
+
+    public static class ProductA implements Product{
+
+    }
+
+    @Override
+    Product produce() {
+        return new ProductA();
+    }
+}
+
+class FactoryB extends AbstractFactory{
+
+    public static class ProductB implements Product{
+
+    }
+
+    @Override
+    Product produce() {
+        return new ProductB();
+    }
 }
