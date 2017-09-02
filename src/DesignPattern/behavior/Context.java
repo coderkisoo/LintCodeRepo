5169a6a137cd01e10573eddacd6524e95eb5b157
CoderKiSoo <coderkisoo@gmail.com>
2017-09-02 23:21:02 +0800

    Commit: 策略模式，不需要写太多的if else，将算法单独封装，可以单独替换，独立于客户产生变换

---
+++ src/DesignPattern/behavior/Context.java
@@ -0,0 +1,41 @@
+package DesignPattern.behavior;
+
+/**
+ * Created by kangqizhou on 2017/9/2.
+ */
+
+/***
+ * 策略模式，不需要写太多的if else，将算法单独封装，可以单独替换，独立于客户产生变换
+ */
+public class Context {
+    private Strategy strategy;
+
+    public void setStrategy(Strategy strategy){
+        this.strategy = strategy;
+    }
+
+    public void caculate(){
+        strategy.strategy();
+    }
+
+}
+
+interface Strategy{
+    void strategy();
+}
+
+class StategyA implements Strategy{
+
+    @Override
+    public void strategy() {
+        System.out.println("strategyA");
+    }
+}
+
+class StrategyB implements Strategy{
+
+    @Override
+    public void strategy() {
+        System.out.println("strategyB");
+    }
+}
