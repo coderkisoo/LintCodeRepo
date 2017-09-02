66baa6344226028a731e2d273269c826dae46b96
CoderKiSoo <coderkisoo@gmail.com>
2017-09-02 19:42:52 +0800

    Commit: FlyWeightFactory负责创建和管理享元单元，当一个客户端请求时，工厂需要检查当前对象池中是否有符合条件的对象，如果有，就返回已经存在的对象，如果没有，则创建一个新对象，

---
+++ src/DesignPattern/struct/Flyweight.java
@@ -0,0 +1,15 @@
+package DesignPattern.struct;
+
+/**
+ * Created by kangqizhou on 2017/9/2.
+ */
+
+/***
+ * FlyWeightFactory负责创建和管理享元单元，当一个客户端请求时，工厂需
+ * 要检查当前对象池中是否有符合条件的对象，如果有，就返回已经存在的对象，如果没有，则创建一个新对象，
+ */
+public class Flyweight {
+    public static void main(String[] args){
+
+    }
+}
