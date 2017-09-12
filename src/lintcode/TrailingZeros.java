605597b98e6408e6ede4da59ea35e07b5d7c675f
CoderKiSoo <coderkisoo@gmail.com>
2017-09-12 18:57:46 +0800

    Commit: 计算尾部的零。 思路：  * 一个n的阶乘中0的个数，与5的倍数的个数有关。  * 5的非5数呗就是一个0  * 5的5倍数就是两个0  * 以此类推  * 再进行简化后可知，一个n/5 就是当前阶乘中0的个数

---
+++ src/lintcode/TrailingZeros.java
@@ -0,0 +1,33 @@
+package lintcode;
+
+/**
+ * Created by kangqizhou on 2017/9/12.
+ */
+
+/**
+ * 计算尾部的零。
+ * 思路：
+ * 一个n的阶乘中0的个数，与5的倍数的个数有关。
+ * 5的非5数呗就是一个0
+ * 5的5倍数就是两个0
+ * 以此类推
+ * 再进行简化后可知，一个n/5 就是当前阶乘中0的个数
+ */
+public class TrailingZeros {
+    /*
+     * param n: As desciption
+     * return: An integer, denote the number of trailing zeros in n!
+     */
+    public long trailingZeros(long n) {
+        // write your code here
+        long temp = n/5;
+        long count = 0;
+        while(temp!=0){
+            count+= temp;
+            temp /= 5;
+        }
+        return count;
+
+    }
+};
+
